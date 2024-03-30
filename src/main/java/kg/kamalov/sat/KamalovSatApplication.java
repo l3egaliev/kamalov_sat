0e7781207de518ce2083ac195fc4aebf2aea62a8
Rocky <tedrocky@gmail.com>
2024-03-31 00:47:36 +0300

    start

---
+++ src/main/java/kg/kamalov/sat/KamalovSatApplication.java
@@ -0,0 +1,13 @@
+package kg.kamalov.sat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamalovSatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamalovSatApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kg/kamalov/sat/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package kg.kamalov.sat.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+}
